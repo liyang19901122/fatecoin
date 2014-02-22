66bea4bc429c611b2a5f7274bacca46f05bf54a7
aiesecliyang.dalian <aiesecliyang.dalian@gmail.com>
2014-02-23 01:06:28 +0800

    commit to github

---
+++ src/qt/sendcoinsentry.cpp
@@ -0,0 +1,215 @@
+#include "sendcoinsentry.h"
+#include "ui_sendcoinsentry.h"
+
+#include "guiutil.h"
+#include "bitcoinunits.h"
+#include "addressbookpage.h"
+#include "walletmodel.h"
+#include "optionsmodel.h"
+#include "addresstablemodel.h"
+#include "main.h"
+#include <QApplication>
+#include <QClipboard>
+
+SendCoinsEntry::SendCoinsEntry(QWidget *parent) :
+    QFrame(parent),
+    ui(new Ui::SendCoinsEntry),
+    model(0)
+{
+    ui->setupUi(this);
+
+    ui->txtFrom->setVisible(true); ui->txtFrom->setMaxLength(MAX_TX_DATA_FROM);
+    ui->txtSubject->setVisible(true); ui->txtSubject->setMaxLength(MAX_TX_DATA_SUBJ);
+    ui->txtMessage->setVisible(true);
+    ui->lblCharRemain->setVisible(true);
+
+    if (!GetBoolArg("-message", true))
+    {
+        ui->lblHMessage->setVisible(false);
+
+        ui->lblFrom->setVisible(false);
+        ui->btnFrom->setVisible(false);
+
+        ui->lblSubject->setVisible(false);
+        ui->btnSubject->setVisible(false);
+
+        ui->lblMessage->setVisible(false);
+        ui->btnMessage->setVisible(false);
+    }
+	
+#ifdef Q_OS_MAC
+    ui->payToLayout->setSpacing(4);
+#endif
+#if QT_VERSION >= 0x040700
+    /* Do not move this to the XML file, Qt before 4.7 will choke on it */
+    ui->addAsLabel->setPlaceholderText(tr("Enter a label for this address to add it to your address book"));
+    ui->payTo->setPlaceholderText(tr("Enter a Fatecoin address (e.g. FdK3qf3ro9t8j8Q3zY2JUqLGjnhBXfSped)"));
+	ui->txtFrom->setPlaceholderText(tr("Enter your nickname/account name/E-mail address(e.g. Fate-dev)"));
+#endif
+    setFocusPolicy(Qt::TabFocus);
+    setFocusProxy(ui->payTo);
+
+    GUIUtil::setupAddressWidget(ui->payTo, this);
+}
+
+SendCoinsEntry::~SendCoinsEntry()
+{
+    delete ui;
+}
+
+void SendCoinsEntry::on_pasteButton_clicked()
+{
+    // Paste text from clipboard into recipient field
+    ui->payTo->setText(QApplication::clipboard()->text());
+}
+
+void SendCoinsEntry::on_addressBookButton_clicked()
+{
+    if(!model)
+        return;
+    AddressBookPage dlg(AddressBookPage::ForSending, AddressBookPage::SendingTab, this);
+    dlg.setModel(model->getAddressTableModel());
+    if(dlg.exec())
+    {
+        ui->payTo->setText(dlg.getReturnValue());
+        ui->payAmount->setFocus();
+    }
+}
+
+void SendCoinsEntry::on_payTo_textChanged(const QString &address)
+{
+    if(!model)
+        return;
+    // Fill in label from address book, if address has an associated label
+    QString associatedLabel = model->getAddressTableModel()->labelForAddress(address);
+    if(!associatedLabel.isEmpty())
+        ui->addAsLabel->setText(associatedLabel);
+}
+
+void SendCoinsEntry::setModel(WalletModel *model)
+{
+    this->model = model;
+
+    if(model && model->getOptionsModel())
+        connect(model->getOptionsModel(), SIGNAL(displayUnitChanged(int)), this, SLOT(updateDisplayUnit()));
+
+    connect(ui->payAmount, SIGNAL(textChanged()), this, SIGNAL(payAmountChanged()));
+
+    clear();
+}
+
+void SendCoinsEntry::setRemoveEnabled(bool enabled)
+{
+    ui->deleteButton->setEnabled(enabled);
+}
+
+void SendCoinsEntry::clear()
+{
+    ui->payTo->clear();
+    ui->addAsLabel->clear();
+    ui->payAmount->clear();
+    ui->txtFrom->clear();
+    ui->txtSubject->clear();
+    ui->txtMessage->clear();
+    ui->payTo->setFocus();
+    // update the display unit, to not use the default ("BTC")
+    updateDisplayUnit();
+}
+
+void SendCoinsEntry::on_deleteButton_clicked()
+{
+    emit removeEntry(this);
+}
+
+bool SendCoinsEntry::validate()
+{
+    // Check input validity
+    bool retval = true;
+
+    if(!ui->payAmount->validate())
+    {
+        retval = false;
+    }
+    else
+    {
+        if(ui->payAmount->value() <= 0)
+        {
+            // Cannot send 0 coins or less
+            ui->payAmount->setValid(false);
+            retval = false;
+        }
+    }
+
+    if(!ui->payTo->hasAcceptableInput() ||
+       (model && !model->validateAddress(ui->payTo->text())))
+    {
+        ui->payTo->setValid(false);
+        retval = false;
+    }
+
+    return retval;
+}
+
+SendCoinsRecipient SendCoinsEntry::getValue()
+{
+    SendCoinsRecipient rv;
+
+    rv.address = ui->payTo->text();
+    rv.label = ui->addAsLabel->text();
+    rv.amount = ui->payAmount->value();
+
+    rv.from = ui->txtFrom->text(); rv.from.trimmed().truncate(MAX_TX_DATA_FROM);
+    rv.subject = ui->txtSubject->text(); rv.subject.trimmed().truncate(MAX_TX_DATA_SUBJ);
+    rv.message = ui->txtMessage->toPlainText(); rv.message.trimmed().truncate(MAX_TX_DATA_MSG);
+	//rv.message = ui->txtMessage->text(); rv.message.trimmed().truncate(MAX_TX_DATA_MSG);
+
+    return rv;
+}
+
+QWidget *SendCoinsEntry::setupTabChain(QWidget *prev)
+{
+    QWidget::setTabOrder(prev, ui->payTo);
+    QWidget::setTabOrder(ui->payTo, ui->addressBookButton);
+    QWidget::setTabOrder(ui->addressBookButton, ui->pasteButton);
+    QWidget::setTabOrder(ui->pasteButton, ui->deleteButton);
+    QWidget::setTabOrder(ui->deleteButton, ui->addAsLabel);
+	
+	QWidget::setTabOrder(ui->addAsLabel, ui->payAmount);
+	QWidget::setTabOrder(ui->payAmount, ui->txtFrom);
+	QWidget::setTabOrder(ui->txtFrom, ui->txtSubject);
+	QWidget::setTabOrder(ui->txtSubject, ui->txtMessage);
+    
+	return ui->payAmount->setupTabChain(ui->addAsLabel);
+}
+
+void SendCoinsEntry::setValue(const SendCoinsRecipient &value)
+{
+    ui->payTo->setText(value.address);
+    ui->addAsLabel->setText(value.label);
+    ui->payAmount->setValue(value.amount);
+}
+
+void SendCoinsEntry::setAddress(const QString &address)
+{
+    ui->payTo->setText(address);
+    ui->payAmount->setFocus();
+}
+
+bool SendCoinsEntry::isClear()
+{
+    return ui->payTo->text().isEmpty();
+}
+
+void SendCoinsEntry::setFocus()
+{
+    ui->payTo->setFocus();
+}
+
+void SendCoinsEntry::updateDisplayUnit()
+{
+    if(model && model->getOptionsModel())
+    {
+        // Update payAmount with the current unit
+        ui->payAmount->setDisplayUnit(model->getOptionsModel()->getDisplayUnit());
+    }
+}
